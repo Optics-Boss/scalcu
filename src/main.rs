65d6a2249522d12988425260a338b6b032c183d1
Optics-Boss <->
2024-03-21 21:27:20 +0100

    Started with a calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,89 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{button, column, text, text_input, pick_list};
+use iced::{Alignment, Element, Sandbox, Settings};
+
+pub fn main() -> iced::Result {
+    Counter::run(Settings::default())
+}
+
+struct Counter {
+    input: String,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Temperature {
+    Fahrenheit,
+    Celsius,
+    Kelvin,
+    Other,
+}
+
+impl Temperature {
+    const ALL: [Temperature; 4] = [
+        Temperature::Fahrenheit,
+        Temperature::Celsius,
+        Temperature::Kelvin,
+        Temperature::Other,
+    ];
+}
+
+impl std::fmt::Display for Temperature {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Temperature::Fahrenheit => "Fahrenheit",
+                Temperature::Celsius => "Celsius",
+                Temperature::Kelvin => "Kelvin",
+                Temperature::Other => "Other",
+            }
+        )
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    ChangeTextInput(String),
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self { 
+            input: "".into(), 
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("Scalcu - Calculator")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::ChangeTextInput(value) => {
+                self.input = value.into();
+            }
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        column![
+            text("From : ").size(25),
+            button("0").on_press(Message::ChangeTextInput("0".to_string())),
+            button("1").on_press(Message::ChangeTextInput("1".to_string())),
+            button("2").on_press(Message::ChangeTextInput("2".to_string())),
+            button("3").on_press(Message::ChangeTextInput("3".to_string())),
+            button("4").on_press(Message::ChangeTextInput("4".to_string())),
+            button("5").on_press(Message::ChangeTextInput("5".to_string())),
+            button("6").on_press(Message::ChangeTextInput("6".to_string())),
+            button("7").on_press(Message::ChangeTextInput("7".to_string())),
+            button("8").on_press(Message::ChangeTextInput("8".to_string())),
+            button("9").on_press(Message::ChangeTextInput("9".to_string())),
+        ]
+        .padding(20)
+        .align_items(Alignment::Center)
+        .into()
+    }
+
 }
