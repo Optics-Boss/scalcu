670335db74b5109684c36fef31b8a7e595dc7f38
Optics-Boss <->
2024-03-22 14:51:16 +0100

    Added rows and stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use iced::widget::{button, column, text, text_input, pick_list};
+use iced::widget::{button, column, text, row, container};
 use iced::{Alignment, Element, Sandbox, Settings};
 
 pub fn main() -> iced::Result {
@@ -68,22 +68,43 @@ impl Sandbox for Counter {
     }
 
     fn view(&self) -> Element<Message> {
-        column![
-            text("From : ").size(25),
-            button("0").on_press(Message::ChangeTextInput("0".to_string())),
-            button("1").on_press(Message::ChangeTextInput("1".to_string())),
-            button("2").on_press(Message::ChangeTextInput("2".to_string())),
-            button("3").on_press(Message::ChangeTextInput("3".to_string())),
-            button("4").on_press(Message::ChangeTextInput("4".to_string())),
-            button("5").on_press(Message::ChangeTextInput("5".to_string())),
-            button("6").on_press(Message::ChangeTextInput("6".to_string())),
-            button("7").on_press(Message::ChangeTextInput("7".to_string())),
-            button("8").on_press(Message::ChangeTextInput("8".to_string())),
-            button("9").on_press(Message::ChangeTextInput("9".to_string())),
-        ]
-        .padding(20)
-        .align_items(Alignment::Center)
-        .into()
+        
+        let header = container(
+            row![
+                 text("todos")
+            ]
+            .align_items(Alignment::Center)
+        );
+
+        let left =  container (column![
+                button("0").on_press(Message::ChangeTextInput("0".to_string())),
+                button("1").on_press(Message::ChangeTextInput("1".to_string())),
+                button("2").on_press(Message::ChangeTextInput("2".to_string())),
+                button("3").on_press(Message::ChangeTextInput("3".to_string())),
+            ]
+            .padding(20)
+            .align_items(Alignment::Center)
+        );
+
+        let middle = container (column![
+                button("4").on_press(Message::ChangeTextInput("0".to_string())),
+                button("5").on_press(Message::ChangeTextInput("1".to_string())),
+                button("6").on_press(Message::ChangeTextInput("2".to_string())),
+            ]
+            .padding(20)
+            .align_items(Alignment::Center)
+        );
+
+        let right = container (column![
+                button("7").on_press(Message::ChangeTextInput("1".to_string())),
+                button("8").on_press(Message::ChangeTextInput("2".to_string())),
+                button("9").on_press(Message::ChangeTextInput("3".to_string())),
+            ]
+            .padding(20)
+            .align_items(Alignment::Center)
+        );
+
+        column![header, row![left, middle, right]].into()
     }
 
 }
