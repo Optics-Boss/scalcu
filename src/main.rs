db041b528e0ad9e63262df3f8eab548c98cf7aef
Optics-Boss <->
2024-03-22 20:53:22 +0100

    added clear and arithmatic not finished yet

--- src/main.rs
+++ src/main.rs
@@ -7,43 +7,33 @@ pub fn main() -> iced::Result {
 
 struct Counter {
     input: String,
+    arithmetic: Arithmetic,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub enum Temperature {
-    Fahrenheit,
-    Celsius,
-    Kelvin,
+pub enum Arithmetic {
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
     Other,
 }
 
-impl Temperature {
-    const ALL: [Temperature; 4] = [
-        Temperature::Fahrenheit,
-        Temperature::Celsius,
-        Temperature::Kelvin,
-        Temperature::Other,
+impl Arithmetic {
+    const ALL: [Arithmetic; 5] = [
+        Arithmetic::Addition,
+        Arithmetic::Subtraction,
+        Arithmetic::Multiplication,
+        Arithmetic::Division,
+        Arithmetic::Other,
     ];
-}
-
-impl std::fmt::Display for Temperature {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Temperature::Fahrenheit => "Fahrenheit",
-                Temperature::Celsius => "Celsius",
-                Temperature::Kelvin => "Kelvin",
-                Temperature::Other => "Other",
-            }
-        )
-    }
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     ChangeTextInput(String),
+    SetArithmetic(Arithmetic),
+    Clear(),
 }
 
 impl Sandbox for Counter {
@@ -51,7 +41,8 @@ impl Sandbox for Counter {
 
     fn new() -> Self {
         Self { 
-            input: "".into(), 
+            input: "".to_owned(), 
+            arithmetic: Arithmetic::Other,
         }
     }
 
@@ -62,7 +53,13 @@ impl Sandbox for Counter {
     fn update(&mut self, message: Message) {
         match message {
             Message::ChangeTextInput(value) => {
-                self.input = value.into();
+                self.input += &value;
+            },
+            Message::SetArithmetic(arithmetic) => {
+                self.arithmetic = arithmetic;
+            }
+            Message::Clear() => {
+                self.input = "".to_string();
             }
         }
     }
@@ -76,7 +73,20 @@ impl Sandbox for Counter {
             .align_items(Alignment::Center)
         );
 
-        let left =  container (column![
+        let settings = container(
+            row![
+                button("C").on_press(Message::Clear()),
+                button("CE").on_press(Message::Clear()),
+                button("%").on_press(Message::SetArithmetic(Arithmetic::Division)),
+                button("X").on_press(Message::SetArithmetic(Arithmetic::Multiplication)),
+                button("+").on_press(Message::SetArithmetic(Arithmetic::Addition)),
+                button("-").on_press(Message::SetArithmetic(Arithmetic::Subtraction)),
+                button("=").on_press(Message::ChangeTextInput("1".to_string())),
+            ]
+        );
+
+        let left = container(
+            column![
                 button("0").on_press(Message::ChangeTextInput("0".to_string())),
                 button("1").on_press(Message::ChangeTextInput("1".to_string())),
                 button("2").on_press(Message::ChangeTextInput("2".to_string())),
@@ -87,24 +97,25 @@ impl Sandbox for Counter {
         );
 
         let middle = container (column![
-                button("4").on_press(Message::ChangeTextInput("0".to_string())),
-                button("5").on_press(Message::ChangeTextInput("1".to_string())),
-                button("6").on_press(Message::ChangeTextInput("2".to_string())),
+                button("4").on_press(Message::ChangeTextInput("4".to_string())),
+                button("5").on_press(Message::ChangeTextInput("5".to_string())),
+                button("6").on_press(Message::ChangeTextInput("6".to_string())),
             ]
             .padding(20)
             .align_items(Alignment::Center)
         );
 
         let right = container (column![
-                button("7").on_press(Message::ChangeTextInput("1".to_string())),
-                button("8").on_press(Message::ChangeTextInput("2".to_string())),
-                button("9").on_press(Message::ChangeTextInput("3".to_string())),
+                button("7").on_press(Message::ChangeTextInput("7".to_string())),
+                button("8").on_press(Message::ChangeTextInput("8".to_string())),
+                button("9").on_press(Message::ChangeTextInput("9".to_string())),
             ]
             .padding(20)
             .align_items(Alignment::Center)
         );
 
-        column![header, row![left, middle, right]].into()
+
+        column![header, settings, row![left, middle, right]].into()
     }
 
 }
