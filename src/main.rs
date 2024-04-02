eb744570d26dc24bf7d1a3ba61355e3125c0ee48
Optics-Boss <->
2024-04-02 23:11:55 +0200

    Added the basic features back in again

--- src/main.rs
+++ src/main.rs
@@ -16,22 +16,26 @@ fn App() -> Element {
         link { rel: "stylesheet", href: "main.css" }
         h1 { class: "white", "{result_1}"}
         div { id: "numbers__left",
-            button { onclick: move |_| result_1.set("7".to_string()), "7"}
-            button { onclick: move |_| result_1.set("4".to_string()), "4"}
-            button { onclick: move |_| result_1.set("1".to_string()), "1"}
-            button { onclick: move |_| result_1.set("0".to_string()), "0"}
+            button { onclick: move |_| result_1.write().push('7'), "7"}
+            button { onclick: move |_| result_1.write().push('4'), "4"}
+            button { onclick: move |_| result_1.write().push('1'), "1"}
+            button { onclick: move |_| result_1.write().push('0'), "0"}
         }
 
         div { id: "numbers__center",
-            button { onclick: move |_| result_1.set("8".to_string()), "8"}
-            button { onclick: move |_| result_1.set("5".to_string()), "5"}
-            button { onclick: move |_| result_1.set("2".to_string()), "2"}
+            button { onclick: move |_| result_1.write().push('8'), "8"}
+            button { onclick: move |_| result_1.write().push('5'), "5"}
+            button { onclick: move |_| result_1.write().push('2'), "2"}
         }
 
         div { id: "numbers__right",
-            button { onclick: move |_| result_1.set("9".to_string()), "9"}
-            button { onclick: move |_| result_1.set("6".to_string()), "6"}
-            button { onclick: move |_| result_1.set("3".to_string()), "3"}
+            button { onclick: move |_| result_1.write().push('9'), "9"}
+            button { onclick: move |_| result_1.write().push('6'), "6"}
+            button { onclick: move |_| result_1.write().push('3'), "3"}
+        }
+
+        div { id: "other",
+            button { onclick: move |_| result_1.set("".to_string()), "C"}
         }
     }
 }
